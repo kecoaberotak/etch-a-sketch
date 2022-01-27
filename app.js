08558e0cacf2ff2ce45a814becb71e9787566e99
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-27 15:07:35 +0700

    edit app.js & style.css

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const grid = document.querySelector('.grid');
-let squareSize = 16;
+let squareSize = 8;
 
 createGrid(squareSize);
 
-function createDiv(divSize) {
+function createDiv(divSize){
 	const div = document.createElement('div');
 	div.classList.add('box');
 	div.style.width = `${divSize}px`;
 	div.style.height = `${divSize}px`;
+
+	return div;
 }
 
-
-function createGrid(gridSize) {
-	for(let i = 0; i < gridSize; i++){
-		for(let j = 0; j < gridSize; j++){
-			grid.appendChild(createDiv(grid.client / gridSize));
+function createGrid(gridSize){
+	for (let i = 0; i < gridSize; i++){
+		for (let j = 0; j < gridSize; j++){
+			grid.appendChild(createDiv(grid.clientWidth / gridSize));
 		}
 	}
 }
