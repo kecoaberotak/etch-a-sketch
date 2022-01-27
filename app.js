69038f643c4b813db646071323b8abafc2344bd8
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-27 15:59:07 +0700

    edit semua file

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ let squareSize = 8;
 
 createGrid(squareSize);
 
-// membuat grid (div)
-function createDiv(divSize){
+function createDiv(divSize) {
 	const div = document.createElement('div');
 	div.classList.add('box');
 	div.style.width = `${divSize}px`;
@@ -13,19 +12,10 @@ function createDiv(divSize){
 	return div;
 }
 
-// membuat grid dan menyimpannya kedalam class grid
-function createGrid(gridSize){
-	for (let i = 0; i < gridSize; i++){
-		for (let j = 0; j < gridSize; j++){
+function createGrid(gridSize) {
+	for(let i = 0; i < gridSize; i++){
+		for(let j = 0; j < gridSize; j++){
 			grid.appendChild(createDiv(grid.clientWidth / gridSize));
 		}
 	}
 }
-
-// Used event delegation to target children of the grid
-grid.addEventListener('mouseover', function (e) {
-  // Add the "active" class to only divs with a "box" class
-  if (e.target.matches('.box')) {
-    e.target.classList.add('active');
-  }
-});
