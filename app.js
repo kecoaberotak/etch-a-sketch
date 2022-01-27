e2b337eef592955dac98a940a5465fc54c4a9360
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-27 16:14:04 +0700

    edit style.css & app.js

--- app.js
+++ app.js
@@ -19,3 +19,10 @@ function createGrid(gridSize) {
 		}
 	}
 }
+
+grid.addEventListener('mouseover', function (e) {
+	// Add the "active" class to only divs with a "box" class
+	if (e.target.matches('.box')) {
+	  e.target.classList.add('active');
+	}
+  });
